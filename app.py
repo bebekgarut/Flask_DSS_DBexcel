ac310b6ba48063f11c79616bee191244ce496b03
ikrammmmmmm <157960444+bebekgarut@users.noreply.github.com>
2024-11-19 19:26:04 +0700

    alternatif

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 db_kriteria = "database/tb_kriteria.xlsx"
 db_ahli = "database/tb_ahli.xlsx"
 db_swara = "database/tb_swara.xlsx"
+db_alternatif = "database/tb_alternatif.xlsx"
 
 def read_kriteria():
     return pd.read_excel(db_kriteria)
@@ -33,6 +34,22 @@ def read_swara():
 
 def save_swara(df):
     df.to_excel(db_swara, index=False)
+    
+def read_alternatif():
+    return pd.read_excel(db_alternatif)
+
+def save_alternatif(df):
+    df.to_excel(db_alternatif, index=False)
+    
+def generate_kode_alternatif(df):
+    if df.empty:
+        return "A1"
+    
+    last_code = df["kode_alternatif"].iloc[-1]
+    last_number = int(last_code[1:])
+    new_number = last_number + 1
+    
+    return f"A{new_number}"
 
 @app.route("/")
 def index():
@@ -44,11 +61,12 @@ def index():
 def tambah():
     if request.method == 'POST':
         kriteria = request.form["kriteria"]
+        jenis_kriteria = request.form["jenis_kriteria"]
         df = read_kriteria()
-        df = df._append({"kriteria" : kriteria}, ignore_index=True)
+        df = df._append({"kriteria" : kriteria, "jenis_kriteria":jenis_kriteria}, ignore_index=True)
         save_kriteria(df)
         return redirect(url_for("index"))
-    return render_template("tambah.jinja")
+    return render_template("kriteria/tambah.jinja")
 
 @app.route("/edit/<int:id>", methods=['GET', 'POST'])
 def edit(id):
@@ -57,10 +75,12 @@ def edit(id):
     
     if request.method == 'POST' :
         kriteria = request.form["kriteria"]
+        jenis_kriteria = request.form["jenis_kriteria"]
         df.loc[id, "kriteria"] = kriteria
+        df.loc[id, "jenis_kriteria"] = jenis_kriteria
         save_kriteria(df)
         return redirect(url_for("index"))
-    return render_template("edit.jinja", data=data_to_edit)
+    return render_template("kriteria/edit.jinja", data=data_to_edit)
 
 @app.route("/hapus/<int:id>")
 def hapus(id):
@@ -190,7 +210,82 @@ def swara():
     
     data = swara_df.to_dict(orient="records")
     
-    return render_template("swara/index.jinja", data=data, average_j=average_j)
+    return render_template("swara/index.jinja", data=data)
+
+@app.route('/bobot')
+def bobot():
+    swara_df = read_swara()
+    data_swara = swara_df.to_dict(orient="records")
+    return render_template("/bobot/index.jinja", data=data_swara)
+
+@app.route('/alternatif')
+def alternatif():
+    kriteria_df = read_kriteria()
+    alternatif_df = read_alternatif()
+    
+    kriteria_data = kriteria_df.iloc[:, 0].tolist()
+    
+    alternatif_columns = set(alternatif_df.columns)
+    kriteria_to_alternatif = set(kriteria_data)
+    
+    new_columns = kriteria_to_alternatif - alternatif_columns
+    for col in new_columns:
+        alternatif_df[col]= 0
+    
+    remove_columns = alternatif_columns - kriteria_to_alternatif
+    remove_columns = remove_columns - {'kode_alternatif'} - {'alternatif'}
+    alternatif_df.drop(columns=remove_columns, inplace=True)
+    
+    save_alternatif(alternatif_df)
+    
+    alternatif__df = read_alternatif()
+    data = alternatif__df.to_dict(orient="records")
+    headers = alternatif__df.columns.tolist()
+    
+    return render_template("alternatif/index.jinja", data=data, headers=headers)
+
+@app.route("/alternatif/tambah", methods=['GET', 'POST'])
+def tambah_alternatif():
+    alternatif_df = read_alternatif()
+    headers = alternatif_df.columns.tolist()
+    
+    if request.method == "POST":
+        new_row = {col : request.form.get(col) for col in headers if col != 'kode_alternatif'}
+        
+        new_row['kode_alternatif'] =generate_kode_alternatif(alternatif_df)
+        
+        alternatif_df = alternatif_df._append(new_row, ignore_index=True)
+        
+        save_alternatif(alternatif_df)
+        
+        return redirect(url_for('alternatif'))
+        
+    return render_template("alternatif/tambah.jinja", headers=headers)
+
+@app.route("/alternatif/edit/<string:kode_alternatif>", methods=['GET' , 'POST'])
+def edit_alternatif(kode_alternatif):
+    alternatif_df = read_alternatif()
+    alternatif_data = alternatif_df[alternatif_df['kode_alternatif']==kode_alternatif].iloc[0]
+    headers = alternatif_df.columns.tolist()
+    
+    if request.method == "POST":
+        for col in headers:
+            if col != 'kode_alternatif':
+                alternatif_df.loc[alternatif_df['kode_alternatif'] == kode_alternatif, col] = request.form.get(col)
+        save_alternatif(alternatif_df)
+        
+        return redirect(url_for('alternatif'))
+    
+    return render_template("alternatif/edit.jinja", data=alternatif_data, headers=headers)
+
+@app.route("/alternatif/hapus/<string:kode_alternatif>")
+def hapus_alternatif(kode_alternatif):
+    alternatif_df = read_alternatif()
+    drop_alternatif = alternatif_df[alternatif_df['kode_alternatif'] == kode_alternatif].index
+    alternatif_df.drop(drop_alternatif, inplace=True)
+    save_alternatif(alternatif_df)
+    return redirect(url_for('alternatif'))
+
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
